a6d7531f64ca83d41c5d0b4579b750bdbd8ea1f1
Rodrigo Ferreira Cavalcante <rodrigofercav@gmail.com>
2024-09-09 18:13:33 -0300

    sytle: basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, NavBarComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'sgp-web';
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './nav-bar.component.html'
+})
+export class NavBarComponent { }
