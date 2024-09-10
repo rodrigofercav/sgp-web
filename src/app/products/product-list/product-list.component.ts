e2fa60e149645540e634c21112e8ab55982155a4
Rodrigo Ferreira Cavalcante <rodrigofercav@gmail.com>
2024-09-10 17:23:20 -0300

    style: add layout for product-list page
    - Only visual changes; no functionalities added yet.

---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  isInactive = true;
+  isExpired = true;
+}
